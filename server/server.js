4793602e38669a4726f84e353a69a845e9b1cc73
hihurmuz <hihurmuz@gmail.com>
2020-06-06 09:35:11 +0300

    server created. not all functions added, but existing ones are working.

---
+++ server/server.js
@@ -0,0 +1,46 @@
+const express = require("express");
+const morgan = require("morgan");
+const dotenv = require("dotenv");
+const cors = require("cors");
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+
+const productRoutes = require("./rotues/product");
+const userMethods = require("./rotues/authenticaton")
+
+const app= express();
+
+app.use(cors());
+app.use(morgan("dev"));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extends:false}));
+
+app.use("/api",productRoutes);
+app.use("/api",userMethods);
+
+dotenv.config();
+
+mongoose.connect(process.env.DATABASE,{
+    useNewUrlParser:true,
+    useUnifiedTopology: true 
+},err=>{
+    if(err){
+        console.log(err);
+        
+    }else{
+        console.log("connected to mongodb  ......");
+        
+    }
+}
+
+)
+
+app.listen(8080, err=>{
+    if(err){
+        console.log(err);
+        
+    }else{
+        console.log("8080 port listen .....");
+        
+    }
+})
