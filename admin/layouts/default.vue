e94028f84c737d7a0e865ef435ec9cdcea23c29e
hihurmuz <hihurmuz@gmail.com>
2020-06-06 11:25:56 +0300

    admin created and navbar added

---
+++ admin/layouts/default.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <TheNavbar/>
+    <nuxt />
+  </div>
+</template>
+<script>
+import TheNavbar from   "../components/TheNavbar"
+export default {
+  components:{
+    TheNavbar
+  }
+}
+</script>
+<style>
+
+</style>
